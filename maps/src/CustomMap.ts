e38243e06d781ac8485af8635f9e545489abdc56
Manish <manish@twurs.com>
2023-09-26 07:20:27 +0530

    Added marker

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -23,13 +23,21 @@ export class CustomMap {
     );
   }
 
-  addMarket(mappable: Mappable): void {
-    new google.maps.Marker({
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.long,
       },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'hi there',
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
